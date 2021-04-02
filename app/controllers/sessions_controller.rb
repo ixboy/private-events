c1ba106f079d8cd423700144698c4343b88d8552
ixboy <ismaelixboy@gmail.com>
2021-04-03 02:31:37 +0300

    add current user model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user.present? && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_path, notice: "Logged in successfully"
+      redirect_to root_path, notice: "Logged in successfully as: #{user.username}"
     else
       flash[:alert] = "Invalid email or password"
       render :new

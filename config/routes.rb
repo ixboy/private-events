cc3ca8d258b771f4d0b2c8238f4d2a519c02b449
ixboy <ismaelixboy@gmail.com>
2021-04-02 03:42:13 +0300

    fix registration controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
   root 'main#index'
 end

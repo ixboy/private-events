6c4528ff2733390a23167036be993b1095c54ed6
ixboy <ismaelixboy@gmail.com>
2021-04-02 07:56:36 +0300

    add loggin session

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
   delete 'logout', to: 'sessions#destroy'
+  get "sign_in", to: 'sessions#new'
+  post "sign_in", to: 'sessions#create'
   root 'main#index'
 end

902d1ceb7a00966acafa9f78ed20bb46874bdaf3
ixboy <ismaelixboy@gmail.com>
2021-04-02 04:07:49 +0300

    add password column to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_01_192720) do
+ActiveRecord::Schema.define(version: 2021_04_02_004754) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end

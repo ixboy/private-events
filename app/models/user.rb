1ad4a4d4e3de8550bf97722c3af8a0b842def126
ixboy <ismaelixboy@gmail.com>
2021-04-03 04:29:31 +0300

    start adding assossiations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
     validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }, uniqueness: true
     validates :password, length: { minimum: 4 }, presence: true
     validates :password_confirmation, length: { minimum: 4 }, presence: true
+
+    has_many :events, class_name: 'Event', foreign_key: 'creator_id' # (direct ownership)
+    has_many :events, foreign_key: :user_id 
+      
+  # This is telling that the user has many invitations (direct ownership)
+    has_many :invitations, foreign_key: :attendee
+  # This is telling that the user can attend events and these events can be found through the Invitation table
+    has_many :event_id, through: 'invitations'
 end

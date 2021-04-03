56bb672e61e4b5d136c70b51a809fc612be26f93
ixboy <ismaelixboy@gmail.com>
2021-04-03 03:03:41 +0300

    add password routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'password', to: 'passwords#edit', as: :edit_password
+  patch "password", to: "passwords#update"
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
   delete 'logout', to: 'sessions#destroy'

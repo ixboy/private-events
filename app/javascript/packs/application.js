ea3a38b102724b55c7db166034612e118c8b9011
ixboy <ismaelixboy@gmail.com>
2021-04-04 08:38:05 +0300

    add havascript packs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import 'bootstrap'
+import "channels"
+
+import * as ActiveStorage from "@rails/activestorage"
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
 
 Rails.start()
 Turbolinks.start()

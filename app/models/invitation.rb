5f8a1ba4ab854481395fff85e02ce8bd2df068ee
ixboy <ismaelixboy@gmail.com>
2021-04-06 07:35:59 +0300

    setup event and invitation models

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -3,24 +3,4 @@ class Invitation < ApplicationRecord
   validates_presence_of :event_id
   belongs_to :attendees, foreign_key: 'attendee_id', class_name: 'User'
   belongs_to :event, foreign_key: 'event_id', class_name: 'Event'
-
-  def self.list_attendees(event)
-    where(event_id: event.id)
-      .pluck(:invitee_id)
-  end
-
-  def self.name_attendees(list)
-    User.where({ id: list })
-      .pluck(:username)
-  end
-
-  def self.list_past_invitations(user)
-    Event.past_events.where({ id: where(invitee_id: user.id) })
-      .pluck(:event_name, :id)
-  end
-
-  def self.list_upcoming_invitations(user)
-    Event.upcoming_events.where({ id: where(attendee_id: user.id) })
-      .pluck(:event_name, :id)
-  end
 end

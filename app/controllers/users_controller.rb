6b6cfb74dda5003d723515c10560d3902bf221de
ixboy <ismaelixboy@gmail.com>
2021-04-06 07:33:42 +0300

    edit users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,12 @@
 class UsersController < ApplicationController
   def index; end
+  def show
+    @user = User.find(params[:id])
+
+    my_events = Event.where(creator: Current.user.id)
+    @current_user_event = my_events || [{ title: 'No Personal Events at the Moment' }]
+
+    my_attendance = Invitation.where(attendee_id: Current.user.id).pluck(:event_id)
+    @current_user_attendance = Event.where(id: my_attendance)
+  end
 end

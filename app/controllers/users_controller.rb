911532aec00d7bbef89e5fd4829f5cbf88cc00cc
ixboy <ismaelixboy@gmail.com>
2021-04-08 09:22:29 +0300

    add attended events variable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
-    my_events = Event.where(creator: Current.user.id)
+    my_events = Current.user.events
     @current_user_event = my_events || [{ title: 'No Personal Events at the Moment' }]
 
     my_attendance = Invitation.where(attendee_id: Current.user.id).pluck(:event_id)
     @current_user_attendance = Event.where(id: my_attendance)
+    @attended_events = Event.where(id: my_attendance).where('event_date < ?', DateTime.now)
   end
 end

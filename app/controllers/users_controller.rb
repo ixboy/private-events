48bdbba3cf7c4fcf36057b0cfa81cf2840eb42bb
ixboy <ismaelixboy@gmail.com>
2021-04-08 18:42:14 +0300

    reduce and rename users show variables

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-
-    my_events = Current.user.events
-    @current_user_event = my_events || [{ title: 'No Personal Events at the Moment' }]
-
-    my_attendance = Invitation.where(attendee_id: Current.user.id).pluck(:event_id)
-    @current_user_attendance = Event.where(id: my_attendance)
-    @attended_events = Event.where(id: my_attendance).where('event_date < ?', DateTime.now)
+    @current_user_event = @user.events || [{ title: 'No Personal Events at the Moment' }]
+    user_attendance = Invitation.where(attendee_id: @user.id).pluck(:event_id)
+    @future_events = Event.where(id: user_attendance)
+    @attended_events = Event.where(id: user_attendance).where('event_date < ?', DateTime.now)
   end
 end
